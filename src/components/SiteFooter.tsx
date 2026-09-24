8b8615c9f69158bf95ac97096f008b84e1c27fb1
Arelera <arelcode@gmail.com>
2026-09-24 20:13:20 +0300

    Give arel.dev more visual character

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function SiteFooter() {
   return <footer className="site-footer"><div className="shell footer-inner">
-    <Link href="/">arel.dev</Link>
+    <Link href="/">arel<span>.</span>dev</Link>
     <Link href="/blog/">Guides</Link>
   </div></footer>
 }
